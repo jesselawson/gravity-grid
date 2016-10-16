be3a666e46b7ba109144e040937f569191080fe5
Jesse Lawson <jesse@Jesses-MacBook-Pro.local>
2016-10-16 16:20:58 -0700

    Initial Commit

---
+++ android/src/com/turkey/gravitygrid/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.turkey.gravitygrid;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.turkey.gravitygrid.GravityGrid;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GravityGrid(), config);
+	}
+}
