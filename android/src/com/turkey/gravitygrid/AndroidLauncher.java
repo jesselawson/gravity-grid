1f08815ad65e44755fd9d2774b242858fad52cbd
Jesse Lawson <jesse@lawsonry.com>
2016-11-25 09:15:41 -0800

    Update config to use immersive mode

--- android/src/com/turkey/gravitygrid/AndroidLauncher.java
+++ android/src/com/turkey/gravitygrid/AndroidLauncher.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016 Jesse Lawson. All Rights Reserved. No part of this code may be redistributed, reused, or otherwise used in any way, shape, or form without written permission from the author.
+ */
+
 package com.turkey.gravitygrid;
 
 import android.os.Bundle;
@@ -11,6 +15,8 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		// Remove the android menu from some android devices
+		config.useImmersiveMode = true;
 		initialize(new GravityGrid(), config);
 	}
 }
